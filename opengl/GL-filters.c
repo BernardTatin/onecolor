75e94c06fd466ee5388954dc476f92ba30182704
bernard (pop) tat1 <bernard.tatin@ik.me>
2022-04-12 18:13:43 +0200

    one-color-GL: great refactoring of filters

--- opengl/GL-filters.c
+++ opengl/GL-filters.c
@@ -10,13 +10,14 @@
 #include "GL-Configuration.h"
 #include "GL-filters.h"
 
-void filter_bw_normal1(void) {
+void bw_normal_function(struct _GLFilter *_parameters) {
+    GLPFilterBWNormal *parameters = (GLPFilterBWNormal *)_parameters->parameters;
     int n = mainImage.number_of_pixels;
     fRGB *rgb = mainImage.rgb;
     RGBA *rgba = mainImage.screen_pixels;
-    const float KlR = 0.299f;
-    const float KlG = 0.587f;
-    const float KlB = 0.114f;
+    const float KlR = parameters->KlR;
+    const float KlG = parameters->KlG;
+    const float KlB = parameters->KlB;
 
 
     for (int i=0; i<n; i++, rgb++, rgba++) {
@@ -30,51 +31,21 @@ void filter_bw_normal1(void) {
         rgba->b = cLuma;
     }
     fprintf(stdout, "End of filter 'normal'\n");
+}
+void filter_bw_normal1(void) {
+    bw_normal_function(&bw_normal1);
 }
 
 void filter_bw_normal2(void) {
-    int n = mainImage.number_of_pixels;
-    fRGB *rgb = mainImage.rgb;
-    RGBA *rgba = mainImage.screen_pixels;
-    const float KlR = 0.2126f;
-    const float KlG = 0.7152f;
-    const float KlB = 0.0722f;
-
-    for (int i=0; i<n; i++, rgb++, rgba++) {
-        float luma = roundf(255.0f * (KlR * rgb->r + KlG * rgb->g + KlB * rgb->b));
-        if (luma > 255.0f) {
-            luma = 255.0f;
-        }
-        u8 cLuma = (u8)luma;
-        rgba->r = cLuma;
-        rgba->g = cLuma;
-        rgba->b = cLuma;
-    }
-    fprintf(stdout, "End of filter 'normal'\n");
+    bw_normal_function(&bw_normal2);
 }
 
 void filter_bw_normal3(void) {
-    int n = mainImage.number_of_pixels;
-    fRGB *rgb = mainImage.rgb;
-    RGBA *rgba = mainImage.screen_pixels;
-    const float KlR = 0.2627f;
-    const float KlG = 0.6780f;
-    const float KlB = 0.0593f;
-
-    for (int i=0; i<n; i++, rgb++, rgba++) {
-        float luma = roundf(255.0f * (KlR * rgb->r + KlG * rgb->g + KlB * rgb->b));
-        if (luma > 255.0f) {
-            luma = 255.0f;
-        }
-        u8 cLuma = (u8)luma;
-        rgba->r = cLuma;
-        rgba->g = cLuma;
-        rgba->b = cLuma;
-    }
-    fprintf(stdout, "End of filter 'normal'\n");
+    bw_normal_function(&bw_normal3);
 }
 
-static void filter_bw_medianne(const u8 r, const u8 g, const u8 b) {
+void bw_hard_function(struct _GLFilter *_parameters) {
+    GLPFilterBWHard *parameters = (GLPFilterBWHard *)_parameters->parameters;
     int n = mainImage.number_of_pixels;
     fRGB *rgb = mainImage.rgb;
     RGBA *rgba = mainImage.screen_pixels;
@@ -83,88 +54,77 @@ static void filter_bw_medianne(const u8 r, const u8 g, const u8 b) {
     const float KlB = 0.0593f;
     float minLuma = 300.0f;
     float maxLuma = 0.0f;
-    float *greys = (float *) malloc(n * sizeof(float));
-    float *pGreys = greys;
-
-    for (int i=0; i<n; i++, rgb++, pGreys++) {
-        float luma = roundf(255.0f * (KlR * rgb->r + KlG * rgb->g + KlB * rgb->b));
-        if (luma > 255.0f) {
-            luma = 255.0f;
-        }
-        *pGreys = luma;
-        if (luma > maxLuma) {
-            maxLuma = luma;
-        } else if (luma < minLuma) {
-            minLuma = luma;
-        }
-    }
-    float mid = 0.5f * (maxLuma + minLuma);
-    pGreys = greys;
-    for (int i=0; i<n; i++, pGreys++, rgba++) {
-        if (*pGreys > mid) {
-            rgba->r = (u8)255;
-            rgba->g = (u8)255;
-            rgba->b = (u8)255;
-        } else {
-            rgba->r = (u8)r;
-            rgba->g = (u8)g;
-            rgba->b = (u8)b;
-        }
-    }
-    free(greys);
-    fprintf(stdout, "End of filter 'hard 1'\n");
-}
-
-void filter_bw_hard_black1(void) {
-    filter_bw_medianne(0, 0, 0);
-}
-
-void filter_bw_hard_black2(void) {
-    int n = mainImage.number_of_pixels;
-    fRGB *rgb = mainImage.rgb;
-    RGBA *rgba = mainImage.screen_pixels;
-    const float KlR = 0.2627f;
-    const float KlG = 0.6780f;
-    const float KlB = 0.0593f;
     float average = 0.0f;
     float *greys = (float *) malloc(n * sizeof(float));
     float *pGreys = greys;
+    float mid;
+    float lr = roundf(255.0f * parameters->lightR),
+            lg = roundf(255.0f * parameters->lightG),
+            lb = roundf(255.0f * parameters->lightB),
+            dr = roundf(255.0f * parameters->darkR),
+            dg = roundf(255.0f * parameters->darkG),
+            db = roundf(255.0f * parameters->darkB);
 
-    for (int i=0; i<n; i++, rgb++, pGreys++) {
-        float luma = roundf(255.0f * (KlR * rgb->r + KlG * rgb->g + KlB * rgb->b));
-        if (luma > 255.0f) {
-            luma = 255.0f;
+
+    if (parameters->type == BWH_median) {
+        for (int i=0; i<n; i++, rgb++, pGreys++) {
+            float luma = roundf(255.0f * (KlR * rgb->r + KlG * rgb->g + KlB * rgb->b));
+            if (luma > 255.0f) {
+                luma = 255.0f;
+            }
+            *pGreys = luma;
+            if (luma > maxLuma) {
+                maxLuma = luma;
+            } else if (luma < minLuma) {
+                minLuma = luma;
+            }
         }
-        *pGreys = luma;
-        average += luma;
+        mid = 0.5f * (maxLuma + minLuma);
+    } else {
+        for (int i=0; i<n; i++, rgb++, pGreys++) {
+            float luma = roundf(255.0f * (KlR * rgb->r + KlG * rgb->g + KlB * rgb->b));
+            if (luma > 255.0f) {
+                luma = 255.0f;
+            }
+            *pGreys = luma;
+            average += luma;
+        }
+        mid = average / (float)n;
     }
-    float mid = average / (float)n;
     pGreys = greys;
     for (int i=0; i<n; i++, pGreys++, rgba++) {
         if (*pGreys > mid) {
-            rgba->r = (u8)255;
-            rgba->g = (u8)255;
-            rgba->b = (u8)255;
+            rgba->r = (u8)lr;
+            rgba->g = (u8)lg;
+            rgba->b = (u8)lb;
         } else {
-            rgba->r = (u8)0;
-            rgba->g = (u8)0;
-            rgba->b = (u8)0;
+            rgba->r = (u8)dr;
+            rgba->g = (u8)dg;
+            rgba->b = (u8)db;
         }
     }
     free(greys);
-    fprintf(stdout, "End of filter 'hard 1'\n");
+}
+
+
+void filter_bw_hard_black1(void) {
+    bw_hard_function(&bw_hard_average);
+}
+
+void filter_bw_hard_black2(void) {
+    bw_hard_function(&bw_hard_median);
 }
 
 void filter_bw_hard_blue(void) {
-    filter_bw_medianne(0, 0, 120);
+    bw_hard_function(&bw_hard_averageB);
 }
 
 void filter_bw_hard_red(void) {
-    filter_bw_medianne(120, 0, 0);
+    bw_hard_function(&bw_hard_averageR);
 }
 
 void filter_bw_hard_green(void) {
-    filter_bw_medianne(0, 120, 0);
+    bw_hard_function(&bw_hard_averageG);
 }
 
 void filter_average(void) {
---
+++ opengl/GL_filter_definitions.c
@@ -0,0 +1,125 @@
+//
+// Created by bernard on 12/04/2022.
+//
+
+#include <stdbool.h>
+#include <math.h>
+#include <IL/il.h>
+#include <GL/glut.h>
+
+#include "GL-Configuration.h"
+#include "GL-filters.h"
+
+/*
+ * Normal black and white
+ */
+
+GLPFilterBWNormal p_bw_normal1 = {
+        .KlR = 0.299f,
+        .KlG = 0.587f,
+        .KlB = 0.114f
+};
+GLPFilterBWNormal p_bw_normal2 = {
+        .KlR = 0.2126f,
+        .KlG = 0.7152f,
+        .KlB = 0.0722f
+};
+GLPFilterBWNormal p_bw_normal3 = {
+        .KlR = 0.2627f,
+        .KlG = 0.6780f,
+        .KlB = 0.0593f
+};
+
+GLFilter bw_normal1 = {
+        FT_BW_Simple,
+        bw_normal_function,
+        &p_bw_normal1
+};
+GLFilter bw_normal2 = {
+        FT_BW_Simple,
+        bw_normal_function,
+        &p_bw_normal1
+};
+GLFilter bw_normal3 = {
+        FT_BW_Simple,
+        bw_normal_function,
+        &p_bw_normal1
+};
+
+/*
+ * hard black and white
+ */
+
+GLPFilterBWHard p_bw_hard_average = {
+        .type = BWH_average,
+        .darkR = 0.0f,
+        .darkG = 0.0f,
+        .darkB = 0.0f,
+        .lightR = 1.0f,
+        .lightG = 1.0f,
+        .lightB = 1.0f,
+};
+
+const float dark = 120.0f;
+GLPFilterBWHard p_bw_hard_averageR = {
+        .type = BWH_average,
+        .darkR = dark,
+        .darkG = 0.0f,
+        .darkB = 0.0f,
+        .lightR = 1.0f,
+        .lightG = 1.0f,
+        .lightB = 1.0f,
+};
+GLPFilterBWHard p_bw_hard_averageG = {
+        .type = BWH_average,
+        .darkR = 0.0f,
+        .darkG = dark,
+        .darkB = 0.0f,
+        .lightR = 1.0f,
+        .lightG = 1.0f,
+        .lightB = 1.0f,
+};
+GLPFilterBWHard p_bw_hard_averageB = {
+        .type = BWH_average,
+        .darkR = 0.0f,
+        .darkG = 0.0f,
+        .darkB = dark,
+        .lightR = 1.0f,
+        .lightG = 1.0f,
+        .lightB = 1.0f,
+};
+GLPFilterBWHard p_bw_hard_median = {
+        .type = BWH_median,
+        .darkR = 0.0f,
+        .darkG = 0.0f,
+        .darkB = 0.0f,
+        .lightR = 1.0f,
+        .lightG = 1.0f,
+        .lightB = 1.0f,
+};
+
+GLFilter bw_hard_average = {
+       .type = FT_BW_HARD,
+       .filter_function = bw_hard_function,
+       .parameters = &p_bw_hard_average
+};
+GLFilter bw_hard_averageR = {
+        .type = FT_BW_HARD,
+        .filter_function = bw_hard_function,
+        .parameters = &p_bw_hard_averageR
+};
+GLFilter bw_hard_averageG = {
+        .type = FT_BW_HARD,
+        .filter_function = bw_hard_function,
+        .parameters = &p_bw_hard_averageG
+};
+GLFilter bw_hard_averageB = {
+        .type = FT_BW_HARD,
+        .filter_function = bw_hard_function,
+        .parameters = &p_bw_hard_averageB
+};
+GLFilter bw_hard_median = {
+        .type = FT_BW_HARD,
+        .filter_function = bw_hard_function,
+        .parameters = &p_bw_hard_median
+};
