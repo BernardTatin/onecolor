6f848ec426d5263e3cc4b37f8e7b7eef60ce7706
bernard (pop) tat1 <bernard.tatin@ik.me>
2022-04-25 12:45:34 +0200

    one-color-nuke: more tests with Nuklear

---
+++ Nuklear/Nuklear-src/full-nuklear.c
@@ -0,0 +1,23 @@
+//
+// Created by bernard on 24/04/22.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdarg.h>
+#include <string.h>
+#include <math.h>
+#include <assert.h>
+#include <limits.h>
+#include <time.h>
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#define FULL_NK_IMPLEMENTATION
+
+#include "nuklear-config.h"
+
+#include "nuklear.h"
+#include "nuklear_glfw_gl3.h"
